2cdcb35edca489a661a59d9c04e4726872fbc1f5
X54329 <bobojoe108@gmail.com>
2013-08-24 15:17:41 -0400

    fixed pixel copying issue

--- src/MazeGenerator.py
+++ src/MazeGenerator.py
@@ -1,6 +1,8 @@
 import random
 
-MAX_INDEX = 15
+NUM_COLUMNS = 16
+NUM_ROWS = 16
+
 
 # The maze is represented by a 16*16 2-dimensional array
 
@@ -23,14 +25,16 @@ class Pixel(object):
         for pixel_array in self.parent_maze.pixels:
             try:
                 pixel_index = pixel_array.index(self)
+                print "row " + str(pixel_index)
                 self.pixel_array = pixel_array
+                print "column " + str(self.parent_maze.pixels.index(pixel_array))
             except ValueError:
                 continue
         neighbors = []
         if pixel_index > 0:
             left_neighbor = self.pixel_array[pixel_index-1]
             neighbors.append(left_neighbor)
-        if pixel_index < MAX_INDEX:
+        if pixel_index < NUM_ROWS:
             right_neighbor = self.pixel_array[pixel_index+1]
             neighbors.append(right_neighbor)
         pixel_array_index = self.parent_maze.pixels.index(pixel_array)
@@ -38,15 +42,16 @@ class Pixel(object):
             prev_row = self.parent_maze.pixels[pixel_array_index-1]
             top_neighbor = prev_row[pixel_index]
             neighbors.append(top_neighbor)
-        if pixel_array_index < MAX_INDEX:
+        if pixel_array_index < NUM_COLUMNS:
             next_row = self.parent_maze.pixels[pixel_array_index+1]
             bottom_neighbor = next_row[pixel_index]
             neighbors.append(bottom_neighbor)
+        
         return neighbors
     
 class Maze(object):
     def __init__(self):
-        self.pixels = [[Pixel(self)]*(MAX_INDEX+1)]*(MAX_INDEX+1)
+        self.pixels = [[Pixel(self) for i in range(NUM_ROWS)] for j in range(NUM_COLUMNS)]
         self.current_pixel = None
     def generate(self):
         row = random.randint(0,15)
@@ -65,3 +70,7 @@ class Maze(object):
             else:
                 continue
         pixel.dead_end = True
+
+m = Maze()
+m.generate()
+x = input("done")
