c1fc50c62b5aafa598b3a63291ab65953a393a17
Amit Mizrahi <amizra@outlook.com>
2013-08-24 20:50:44 -0400

    Fix neighbors

--- src/MazeGenerator.py
+++ src/MazeGenerator.py
@@ -2,11 +2,8 @@ import random
 
 NUM_COLUMNS = 16
 NUM_ROWS = 16
-# The maze is represented by a 16*16 2-dimensional array
-
 
 class Pixel(object):
-    # Using index to find rows and columns is too complicated, pixels just remember their row and column
     def __init__(self, maze, row, column):
         self.parent_maze = maze
         self.row = row
@@ -15,12 +12,12 @@ class Pixel(object):
         self.dead_end = False
         self.discovered = False
         self.white = False
-	# Sides start at right-facing and go counter-clockwise
-	# side_open[0] ->
-	# side_open[1] /\
-	# side_open[2] <-
-	# side_open[3] \/
-	self.side_open = [False, False, False, False]
+	    # Sides start at right-facing and go counter-clockwise
+	    # side_open[0] ->
+	    # side_open[1] /\
+	    # side_open[2] <-
+	    # side_open[3] \/
+    	self.side_open = [False, False, False, False]
     def get_row(self):
         return self.row
     def get_column(self):
@@ -29,9 +26,9 @@ class Pixel(object):
         self.discovered = True
         self.white = True
     def get_sides_open(self):
-	return self.side_open
+	    return self.side_open
     def set_side_open(self, side):
-	self.side_open[side] = True
+	    self.side_open[side] = True
     def discovered(self):
         return discovered  
     def get_neighbors(self):
@@ -46,17 +43,17 @@ class Pixel(object):
 ##            except ValueError:
 ##                continue
         neighbors = []
-        if self.row > 0:
-            left_neighbor = self.parent_maze.pixels[self.row - 1][self.column]
-            neighbors.append(left_neighbor)
-        if self.row < NUM_ROWS - 1:
-            right_neighbor = self.parent_maze.pixels[self.row + 1][self.column]
-            neighbors.append(right_neighbor)
         if self.column > 0:
-            top_neighbor = self.parent_maze.pixels[self.row][self.column - 1]
-            neighbors.append(top_neighbor)
+            left_neighbor = self.parent_maze.pixels[self.row][self.column - 1]
+            neighbors.append(left_neighbor)
         if self.column < NUM_COLUMNS - 1:
-            bottom_neighbor = self.parent_maze.pixels[self.row][self.column + 1]
+            right_neighbor = self.parent_maze.pixels[self.row][self.column + 1]
+            neighbors.append(right_neighbor)
+        if self.row > 0:
+            top_neighbor = self.parent_maze.pixels[self.row - 1][self.column]
+            neighbors.append(top_neighbor)
+        if self.row < NUM_ROWS - 1:
+            bottom_neighbor = self.parent_maze.pixels[self.row + 1][self.column]
             neighbors.append(bottom_neighbor)
         return neighbors
     
@@ -75,25 +72,20 @@ class Maze(object):
         neighbors = pixel.get_neighbors()
         random.shuffle(neighbors)
         for neighbor in neighbors:
-            print "row " + str(neighbor.get_row())
-            print "column " + str(neighbor.get_column())
             if neighbor.discovered == False:
-
-		if neighbor.row < pixel.row:
-		    pixel.set_side_open(1)
-		    neighbor.set_side_open(3)
-		if neighbor.row > pixel.row:
-		    pixel.set_side_open(3)
-		    neighbor.set_side_open(1)
-		if neighbor.column < pixel.column:
+                if neighbor.row < pixel.row:
+                    pixel.set_side_open(1)
+                    neighbor.set_side_open(3)
+                if neighbor.row > pixel.row:
+                    pixel.set_side_open(3)
+                    neighbor.set_side_open(1)
+                if neighbor.column < pixel.column:
                     pixel.set_side_open(0)
                     neighbor.set_side_open(2)
                 if neighbor.column > pixel.column:
                     pixel.set_side_open(2)
                     neighbor.set_side_open(0)
-			
                 neighbor.white = True
-#		x = input("next path")
                 self.pixel_procedure(neighbor)
             else:
                 continue
